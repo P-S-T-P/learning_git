604f5b1b02b6c49b88252744c7323e3112bcabcd
Sameer <psameertanayapatro@gmail.com>
2025-09-27 20:23:44 +0530

    script.js updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("himanshu");
 console.log("Sameer");
 console.log("Rishabh");
 console.log("Ankit");
-functon Greeting(){
+function Greeting() {
   console.log("Hii ! Hello ! How Are You ?");
 }
+console.log("script.js Updated");
